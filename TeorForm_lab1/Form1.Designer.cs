8b2425a34cdd603bb8162edebc8a265bb761d558
Алексей Гардер <aleksey_garder@outlook.com>
2019-02-07 22:53:58 +0700

    Add email finding

--- TeorForm_lab1/Form1.Designer.cs
+++ TeorForm_lab1/Form1.Designer.cs
@@ -122,7 +122,7 @@
             // выходToolStripMenuItem
             // 
             this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
-            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
             this.выходToolStripMenuItem.Text = "Выход";
             this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
             // 
---
+++ TeorForm_lab1/RegExpBackend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace TeorForm_lab1
+{
+    public static class RegExpBackend
+    {
+        public static MatchCollection FindAllEmails(string input)
+        {
+            var except = @"(?<=(\s|^))\w[a-z0-9\._\-]*@[a-z0-9\._\-]+(?=(\s|$))";
+            var reg = new Regex(except, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var result = reg.Matches(input);
+
+            return result;
+        }
+    }
+}
