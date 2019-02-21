9bfa9c75f499f78f6437ca32b9701423fc8512e2
Luzyanin Alexey <261-96@mail.ru>
2019-02-21 14:24:54 +0700

    changing front and adding indexes and lengths fo E-mails

--- TeorForm_lab1/EMailForm.cs
+++ TeorForm_lab1/EMailForm.cs
@@ -21,7 +21,8 @@ namespace TeorForm_lab1
         {
             richTextBoxEMailOut.Clear();
             var result = RegExpBackend.FindAllEmails(richTextBoxEMailIn.Text.ToString());
-            foreach (var a in result) richTextBoxEMailOut.AppendText(a + " ");
+            for (int i = 0; i < result.Count; i++) richTextBoxEMailOut.AppendText(result[i].ToString() + " Индекс: " + result[i].Index +
+                " Длина адреса: " + result[i].Length + "\n");
         }
     }
 }
