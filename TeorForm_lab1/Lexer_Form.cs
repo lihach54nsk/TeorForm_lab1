22c1fcc7f665d04d9ff2a0497483481ba296bdc3
Luzyanin Alexey <261-96@mail.ru>
2019-03-17 13:14:42 +0700

    UI

--- TeorForm_lab1/Lexer_Form.cs
+++ TeorForm_lab1/Lexer_Form.cs
@@ -28,6 +28,8 @@ namespace TeorForm_lab1
                     case Lexer.SyntaxValueToken<int> inti: richTextBoxOut.AppendText(inti.Value.ToString() + " - " + inti.SyntaxKind.ToString() + " Позиция - " + inti.SourceTextPosition.ToString() + "\n"); break;
                     case Lexer.SyntaxValueToken<char> charType: richTextBoxOut.AppendText(charType.Value.ToString() + " - " + charType.SyntaxKind.ToString() + " Позиция - " + charType.SourceTextPosition.ToString() + "\n"); break;
                     case Lexer.SyntaxValueToken<double> doubleType: richTextBoxOut.AppendText(doubleType.Value.ToString() + " - " + doubleType.SyntaxKind.ToString() + " Позиция - " + doubleType.SourceTextPosition.ToString() + "\n"); break;
+                    case Lexer.SyntaxIdentifierToken iden: richTextBoxOut.AppendText($"{ iden.IdentifierName} - {iden.SyntaxKind}  Позиция - {iden.SourceTextPosition}\n"); break;
+                    case Lexer.SyntaxTriviaToken trivia: richTextBoxOut.AppendText($"{trivia.SyntaxKind} Позиция - {trivia.SourceTextPosition}"); break;
                 }
             }
         }
