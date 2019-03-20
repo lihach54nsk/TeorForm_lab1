f081131dc7fa9d9032a41cc2e673ce6696e83744
Алексей Гардер <aleksey_garder@outlook.com>
2019-03-20 14:58:22 +0700

    Remove useless code

--- TeorForm_lab1/Program.cs
+++ TeorForm_lab1/Program.cs
@@ -15,19 +15,6 @@ namespace TeorForm_lab1
         [STAThread]
         static void Main()
         {
-            var source = "04949.1232";
-            var parser = new RecursiveDescent.DecimalParser();
-            var result = parser.ParseDecimalConst(new TextData(source), out var warnings);
-            //var tokens = Lexer.Lexer.GetTokens(new TextData(source));
-
-            var resultStr = new System.Text.StringBuilder();
-            foreach (var item in warnings)
-            {
-                resultStr.AppendLine(item.ToString());
-            }
-
-            Console.WriteLine(resultStr.ToString());
-            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
