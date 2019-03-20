b55bebf5262f91715cd4d7ab66bca06c8a011dcf
Алексей Гардер <aleksey_garder@outlook.com>
2019-03-20 22:16:47 +0700

    Bug fixed

--- TeorForm_lab1/RecursiveDescent/ArithmeticExpressionParser.cs
+++ TeorForm_lab1/RecursiveDescent/ArithmeticExpressionParser.cs
@@ -147,32 +147,6 @@ namespace TeorForm_lab1.RecursiveDescent
             {
                 switch (_source.PeekChar())
                 {
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                    case 'G':
-                    case 'H':
-                    case 'I':
-                    case 'J':
-                    case 'K':
-                    case 'L':
-                    case 'M':
-                    case 'N':
-                    case 'O':
-                    case 'P':
-                    case 'Q':
-                    case 'R':
-                    case 'S':
-                    case 'T':
-                    case 'U':
-                    case 'V':
-                    case 'W':
-                    case 'X':
-                    case 'Y':
-                    case 'Z':
                     case '0':
                     case '1':
                     case '2':
@@ -210,6 +184,32 @@ namespace TeorForm_lab1.RecursiveDescent
                     case '7':
                     case '8':
                     case '9':
+                    case 'A':
+                    case 'B':
+                    case 'C':
+                    case 'D':
+                    case 'E':
+                    case 'F':
+                    case 'G':
+                    case 'H':
+                    case 'I':
+                    case 'J':
+                    case 'K':
+                    case 'L':
+                    case 'M':
+                    case 'N':
+                    case 'O':
+                    case 'P':
+                    case 'Q':
+                    case 'R':
+                    case 'S':
+                    case 'T':
+                    case 'U':
+                    case 'V':
+                    case 'W':
+                    case 'X':
+                    case 'Y':
+                    case 'Z':
                         SaveChar();
                         _source.AdvanceChar();
                         break;
