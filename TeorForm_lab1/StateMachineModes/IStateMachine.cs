f9581220b5848c899e21a8c825d88130fff768bd
Luzyanin Alexey <261-96@mail.ru>
2021-05-02 13:52:04 +0700

    forst version of the ci/c++ comments parser

---
+++ TeorForm_lab1/StateMachineModes/IStateMachine.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TeorForm_lab1.Lexer;
+
+namespace TeorForm_lab1.StateMachineModes
+{
+    public interface IStateMachine
+    {
+        bool Parser(TextData textData, out List<Errors> errorsCollection, out string result);
+        void MakeWarning(string text, char character, int position, ErrorType error);
+    }
+}
---
+++ TeorForm_lab1/StateMachineModes/StateMachineEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace TeorForm_lab1.StateMachineModes
+{
+    enum StateMachineEnum : byte
+    {
+        Start, // 1
+        FirstSlash, // 2
+        SecondSlash, // 3
+        FirstStar, // 4
+        SecondStar, // 5
+        LastSlash, // 6
+        End // K
+    }
+}
